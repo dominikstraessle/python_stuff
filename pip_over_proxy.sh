aaa57ec4e23a352ad7d34e48cbfa8c2357602cf7
Dominik Strässle <dominik.straessle@gibmit.ch>
2018-02-13 16:50:52 +0100

    added script to use pip behind an annoying proxy.

---
+++ pip_over_proxy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#: Title : PIP over Proxy
+#: Date : 2018-02-13
+#: Author : Dominik Strässle
+#: Version : 1.0
+#: Description : Use PIP behind a Proxy
+#: Options : None
+#: Prerequisites : Replace "module", https- and http proxy server
+#: Warning : 
+
+export http_proxy="http://user:pwd@server:port"
+export https_proxy="https://user:pwd@server:port"
+pip install --index-url=https://pypi.python.org/simple/ --trusted-host pypi.python.org "module"
