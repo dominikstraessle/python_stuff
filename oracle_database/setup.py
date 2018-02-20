f112c19e0ac6ddbb339b349db897670d3cbbe56e
Dominik Strässle <dominik.straessle@gibmit.ch>
2018-02-20 15:21:50 +0100

    Context Manager for Oracle Database Connections
    using cx_Oracle

---
+++ oracle_database/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='oracle_database_connection',
+    version='1.0',
+    description='A Context Manager Protocol for Oracle Database access.',
+    author='Dominik Strässle',
+    py_modules=['connection'],
+)
