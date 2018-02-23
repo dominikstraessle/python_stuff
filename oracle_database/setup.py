3f8c261a2fc9fd8c5742f0fbff515f04f4175ed8
Dominik Strässle <dominik.straessle@gibmit.ch>
2018-02-23 14:32:25 +0100

    Delete setup.py

--- oracle_database/setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup
-
-setup(
-    name='oracle_database_connection',
-    version='1.0',
-    description='A Context Manager Protocol for Oracle Database access.',
-    author='Dominik Strässle',
-    py_modules=['connection'],
-)
